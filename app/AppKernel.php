ff42608cc04629f40980ea20bf2ac28957cd3ff4
Alfonso Alba <alfonso@ceronet.es>
2012-05-28 11:56:00 +0200

    Cursosf2GeolocalizacionBundle commit inicial

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Cursosf2\XchangeRatesBundle\Cursosf2XchangeRatesBundle(),
+            new Cursosf2\GeolocalizacionBundle\Cursosf2GeolocalizacionBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Cursosf2/GeolocalizacionBundle/Cursosf2GeolocalizacionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cursosf2\GeolocalizacionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Cursosf2GeolocalizacionBundle extends Bundle
+{
+}
