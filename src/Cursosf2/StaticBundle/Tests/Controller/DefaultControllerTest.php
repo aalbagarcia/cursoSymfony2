c48b6f084f19d8787e1dcc4277113d77806260be
Alfonso Alba <alfonso@ceronet.es>
2012-06-04 17:04:53 +0200

    Test funcional para la portada.

--- src/Cursosf2/StaticBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Cursosf2/StaticBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * This file is part of Cursosf2Application.
+ *
+ * Cursosf2Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cursosf2Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cursosf2Application.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace Cursosf2\StaticBundle\Tests\Controller;
 
@@ -6,12 +22,14 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $crawler = $client->request('GET', '/');
+        $container = $client->getContainer();
+        $limite = $container->getParameter('cursosf2.static_bundle.portada.proximas_reuniones');
+        $this->assertCount($limite, $crawler->filter('section.proximas-reuniones ul li'));
     }
 }
