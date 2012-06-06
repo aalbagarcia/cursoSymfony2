9ca0641c8cd1d6168f4514a47c3a6e95deb88e8a
Alfonso Alba <alfonso@ceronet.es>
2012-06-06 07:54:02 +0200

    Procesando el formulario de registro.

--- src/Cursosf2/UsuariosBundle/Controller/DefaultController.php
+++ src/Cursosf2/UsuariosBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
 use Cursosf2\UsuariosBundle\Entity\Usuario;
 use Cursosf2\UsuariosBundle\Form\Frontend\UsuarioType;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultController extends Controller
 {
@@ -46,10 +47,48 @@ class DefaultController extends Controller
         ));
     }
 
+    /**
+     * Muestra y procesa el formulario de registro.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function registroAction() {
 
         $usuario = new Usuario();
         $formulario = $this->createForm(new UsuarioType(), $usuario);
+
+        $request = $this->getRequest();
+        if($request->getMethod() == 'POST') {
+            //Procesamos el formulario
+            $formulario->bindRequest($request);
+            if ($formulario->isValid()) {
+                // guardar la información en la base de datos
+                $encoder = $this->get('security.encoder_factory')
+                    ->getEncoder($usuario);
+                $usuario->setSalt(md5(time()));
+                $passwordCodificado = $encoder->encodePassword(
+                    $usuario->getPassword(),
+                    $usuario->getSalt()
+                );
+                $usuario->setPassword($passwordCodificado);
+                $usuario->setSlug($usuario->getNombre().time().rand(1,10));
+                $usuario->setFechaAlta(new \DateTime('now'));
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($usuario);
+                $em->flush();
+                $this->get('session')->setFlash('success',
+                    '¡Enhorabuena! Te has registrado correctamente en Cursosf2'
+                );
+                $token = new UsernamePasswordToken(
+                    $usuario,
+                    $usuario->getPassword(),
+                    'usuarios',
+                    $usuario->getRoles()
+                );
+                $this->container->get('security.context')->setToken($token);
+                return $this->redirect($this->generateUrl('homepage'));
+            }
+        }
         return $this->render('Cursosf2UsuariosBundle:Default:registro.html.twig', array('formulario' => $formulario->createView()));
     }
 }
